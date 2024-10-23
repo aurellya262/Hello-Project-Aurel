cd9b8e08eab9bc60463770a85e3167e390bd40de
aurellya262 <aurellyasalsabilla02@gmail.com>
2024-10-23 12:30:04 +0700

    Menambahkan file ke repository

---
+++ aurell.cpp
@@ -0,0 +1,20 @@
+#ifndef SLL_H
+#define SLL_H
+
+typedef struct {
+    char name[100];  // Nama peserta
+    char team[100];  // Nama tim
+    int score;       // Skor
+} infotype;
+
+typedef struct elmtList *adr;
+typedef struct elmtList {
+    infotype info;
+    adr next;
+} elmList;
+
+typedef struct {
+    adr First;
+} List;
+
+#endif
